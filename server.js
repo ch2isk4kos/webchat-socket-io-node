20333b8f0741c2308c04fb5a6bde3971c26ec814
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-07 00:46:02 -0400

    import client utility function and invoke as parameter of socket.io #emit method

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const http = require("http"); // built-in express
 const express = require("express");
 const socket = require("socket.io");
 
+// client import utility functions
+const formatMessage = require("./public/util/messages");
+
 // initialization
 const app = express();
 const server = http.createServer(app);
@@ -17,19 +20,22 @@ app.use(express.static(path.join(__dirname, "public"))); // set static folder to
 // Listeners
 io.on("connection", (socket) => {
   // run server when client connects
-  socket.emit("message", "Welcome to the chatroom!"); // emits a message to newly connected client instance
-  socket.broadcast.emit("message", "New user has entered the chatroom!"); // broadcasts a message to all instances minus client instance
+  socket.emit("message", formatMessage("Chatbot", "Welcome to the chatroom!")); // emits a message to newly connected client instance
+  socket.broadcast.emit(
+    "message",
+    formatMessage("Chatbot", "USER has entered the chatroom!")
+  ); // broadcasts a message to all instances minus client instance
   // io.emit("", "") --> sends message to client
 
   // runs when client instance disconnects
   socket.on("disconnect", () => {
-    io.emit("message", "User has left the chatroom.");
+    io.emit("message", formatMessage("Chatbot", `USER has left the chatroom.`));
   });
 
   // listen for user message submission
   socket.on("userMessage", (message) => {
     console.log("user message received:", message);
-    io.emit("message", message); // emit user message to client
+    io.emit("message", formatMessage("USER", message)); // emit user message to client
   });
 });
 
