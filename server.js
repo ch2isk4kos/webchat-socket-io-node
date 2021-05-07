31bd0fb28263a75503fef67d10dccccde23b3c2d
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-06 21:01:02 -0400

    import nodejs functionality and set static folder to front end as parameter of express #static method

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const path = require("path"); // built-in nodejs fn()
 const express = require("express");
 const app = express();
 
 const PORT = 3000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, "public"))); // set static folder - front end
 
 app.listen(PORT, () => {
   console.log(`Express Server Running: ${PORT}`);
