d010a10aba7bde00e51431c2482788406e1f3602
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-06 21:43:00 -0400

    import express built-in http module and pass express server as parameter to create entry point

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const path = require("path"); // built-in nodejs fn()
+const path = require("path"); // built-in nodejs
+const http = require("http"); // built-in express
 const express = require("express");
+
 const app = express();
+const server = http.createServer(app);
 
 const PORT = 3000 || process.env.PORT;
 
-app.use(express.static(path.join(__dirname, "public"))); // set static folder - front end
+app.use(express.static(path.join(__dirname, "public"))); // set static folder to render html
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Express Server Running: ${PORT}`);
+// });
+
+server.listen(PORT, () => {
   console.log(`Express Server Running: ${PORT}`);
 });
