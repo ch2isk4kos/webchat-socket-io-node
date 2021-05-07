943ed5e04262206ff80fcaa8bb5cebd95454f93d
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-07 15:18:25 -0400

    create utility functions to disconnect a client instance from a chatroom and to query the users of a specified chatroom

--- public/util/users.js
+++ public/util/users.js
@@ -1,16 +1,31 @@
 const users = [];
 
+// client instance
 const joinUser = (id, username, room) => {
   const u = { id, username, room };
   users.push(u);
   return u;
 };
 
+// client instance
 const getCurrentUser = (id) => {
   return users.find((u) => u.id === id);
+};
+
+// client instance
+const disconnectUser = (id) => {
+  const index = users.findIndex((u) => u.id === id);
+  if (index !== -1) return users.splice(index, 1);
+};
+
+// pool
+const getPoolFromRoom = (room) => {
+  return users.filter((u) => u.room === room);
 };
 
 module.exports = {
   joinUser,
   getCurrentUser,
+  disconnectUser,
+  getPoolFromRoom,
 };
