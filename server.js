5621cb6ea4d0c332333f83cb9d2e37c9476051b6
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-06 21:52:00 -0400

    import and initialize socket.io module && add a listener to log a new websocket connection

--- server.js
+++ server.js
@@ -2,16 +2,22 @@
 const path = require("path"); // built-in nodejs
 const http = require("http"); // built-in express
 const express = require("express");
-const socketio = require("socket.io");
+const socket = require("socket.io");
 
 // initialization
 const app = express();
 const server = http.createServer(app);
+const io = socket(server);
 
 const PORT = 3000 || process.env.PORT;
 
 // middleware
 app.use(express.static(path.join(__dirname, "public"))); // set static folder to render html
+
+// run server when client connects
+io.on("connection", (socket) => {
+  console.log("New websocket connected");
+});
 
 // server
 
