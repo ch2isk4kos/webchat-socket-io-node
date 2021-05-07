1191a47b6ebd63b81ca0182b2e19d3c2fa6f5e10
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-07 14:02:34 -0400

    add socket for when client instance joins a chatroom

--- server.js
+++ server.js
@@ -17,25 +17,32 @@ const PORT = 3000 || process.env.PORT;
 // middleware
 app.use(express.static(path.join(__dirname, "public"))); // set static folder to render html
 
-// Listeners
 io.on("connection", (socket) => {
-  // run server when client connects
-  socket.emit("message", formatMessage("Chatbot", "Welcome to the chatroom!")); // emits a message to newly connected client instance
-  socket.broadcast.emit(
-    "message",
-    formatMessage("Chatbot", "USER has entered the chatroom!")
-  ); // broadcasts a message to all instances minus client instance
-  // io.emit("", "") --> sends message to client
+  // run when user joins chatroom
+  socket.on("join", ({ username, room }) => {
+    socket.emit(
+      "message",
+      formatMessage("Chatbot", `Welcome to the ${room} chatroom!`)
+    ); // emits a message to newly connected client instance
+    socket.broadcast.emit(
+      "message",
+      formatMessage("Chatbot", `${username} has entered the chatroom!`)
+    ); // broadcasts a message to all instances minus client instance
+    // io.emit("", "") --> sends message to client
+  });
+
+  // listens for user message submission
+  socket.on("userMessage", (message) => {
+    console.log(`${username} message received:`, message);
+    io.emit("message", formatMessage(`${username}`, message)); // emit user message to client
+  });
 
   // runs when client instance disconnects
   socket.on("disconnect", () => {
-    io.emit("message", formatMessage("Chatbot", `USER has left the chatroom.`));
-  });
-
-  // listen for user message submission
-  socket.on("userMessage", (message) => {
-    console.log("user message received:", message);
-    io.emit("message", formatMessage(`USER`, message)); // emit user message to client
+    io.emit(
+      "message",
+      formatMessage("Chatbot", `${username} has left the chatroom.`)
+    );
   });
 });
 
