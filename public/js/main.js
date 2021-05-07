88eca5250e653fea3547ba476e68ceb92df79433
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-06 22:44:11 -0400

    add listener for user message submission

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,15 @@
-const socket = io();
+// main.js listens for client events to send server.js
+const form = document.getElementById("chat-form");
+const socket = io(); // socket.io script from chat.html
 
 socket.on("message", (message) => {
   console.log("message:", message);
+});
+
+// add listener for form submission
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = e.target.elements.msg.value; // get client instance message string
+  console.log("message to submit:", message);
+  socket.emit("userMessage", message); // emit client instance message to the server
 });
