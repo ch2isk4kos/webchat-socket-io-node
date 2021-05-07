a310b10549d3a4bd28ad959b5dc7464f8c5d2d0a
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-06 21:44:57 -0400

    add comments

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+// imports
 const path = require("path"); // built-in nodejs
 const http = require("http"); // built-in express
 const express = require("express");
+const socketio = require("socket.io");
 
+// initialization
 const app = express();
 const server = http.createServer(app);
 
 const PORT = 3000 || process.env.PORT;
 
+// middleware
 app.use(express.static(path.join(__dirname, "public"))); // set static folder to render html
+
+// server
 
 // app.listen(PORT, () => {
 //   console.log(`Express Server Running: ${PORT}`);
